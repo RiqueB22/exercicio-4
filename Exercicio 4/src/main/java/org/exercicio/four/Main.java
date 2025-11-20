47d0cc48c4caa3bd47122ccfd44b6b7712cce4f6
Rique22 <henrique@cnia.com.br>
2025-11-20 11:28:45 -0300

    Comentando Main.java

--- Exercicio 4/src/main/java/org/exercicio/four/Main.java
+++ Exercicio 4/src/main/java/org/exercicio/four/Main.java
@@ -4,17 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+* Mini dicionario de tradução
+* author: Henrique Bernardo
+* */
+
 public class Main {
     public static void main(String[] args) {
 
+        //Variaveis
         Map<String,String> dicionario = new HashMap<>();
         String palavraIngles;
         String palavraPortugues;
         String opcao = "";
 
+        //loop para o usuario escolher a ação que ele preferir
         while (!opcao.equals("3")){
+            //Input das opções do menu
             Scanner escolha = new Scanner(System.in);
             System.out.println("Digite uma das opções do menu: ");
             System.out.println("1- Digite as palavras em inglês e em portugues para tradução.");
@@ -22,17 +28,22 @@ public class Main {
             System.out.println("3- Sair do Programa.");
             opcao = escolha.nextLine();
 
+            //Checa se tem só numero
             if (opcao.matches(".*[0-9].*")){
+                //Checa qual das opções é
                 switch (opcao){
                     case "1":
+                        //Input para palavra em inglês
                         Scanner input = new Scanner(System.in);
                         System.out.println("Digite sua palavra em inglês: ");
                         palavraIngles = input.nextLine().toLowerCase();
 
+                        //Input para palavra em português
                         Scanner entrada = new Scanner(System.in);
-                        System.out.println("Digite sua palavra em portugues: ");
+                        System.out.println("Digite sua palavra em português: ");
                         palavraPortugues = entrada.nextLine().toLowerCase();
 
+                        //Checa se tem letras minusculas e se tiver acento é palavra em português
                         if(palavraIngles.matches(".*[a-z].*") &&
                                 palavraPortugues.matches(".*[a-záéíóúãõâêôç].*")){
                             dicionario.put(palavraIngles, palavraPortugues);
@@ -42,11 +53,13 @@ public class Main {
                         }
                         break;
                     case "2":
+                        //Input para palavra em inglês
                         Scanner procura = new Scanner(System.in);
                         System.out.println("Digite uma palavra em inglês para traduzir: ");
                         System.out.println(dicionario);
                         palavraIngles = procura.nextLine().toLowerCase();
 
+                        //Checa se tem letras minusculas
                         if(palavraIngles.matches(".*[a-z].*")){
                             System.out.println(dicionario.get(palavraIngles));
                         }else{
